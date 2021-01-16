da869260a00587889687bd0f41f7a710ef44230d
stwrdax <74595001+stwrdax@users.noreply.github.com>
2021-01-16 14:55:56 +0800

    fix on new setup for schoolyear

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
             ]);
         }
         elseif($user->roles->name == 'Super Admin'){
-            $data['schoolyear'] = (Setting::first()->schoolYear ? Setting::first()->schoolYear->name : 0);
+            $data['schoolyear'] = (Setting::first()->schoolYear ? Setting::first()->schoolYear->name : 'Not set');
             $data['students'] = User::where('role_id', '1')->get();
             $data['enrolledStudents'] = User::enrolledStudents();
             $data['instructors'] = User::where('role_id', '4')->get();
